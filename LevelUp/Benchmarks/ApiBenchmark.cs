8e074397fee96e7c50d6590aa6c8d2dc01dae66f
Povilas Šimanskas <p.simanskas@adform.com>
2025-01-24 08:50:12 +0200

    -

--- LevelUp/Benchmarks/ApiBenchmark.cs
+++ LevelUp/Benchmarks/ApiBenchmark.cs
@@ -5,33 +5,29 @@ namespace Benchmarks;
 public class ApiBenchmark
 {
     private readonly HttpClient _httpClient = new();
-    private const string allSimple = "https://localhost:44359/Products/allSimple";
-    private const string allAsSplitQuery = "https://localhost:44359/Products/allAsSplitQuery";
-    private const string allAsNoTracking = "https://localhost:44359/Products/allAsNoTracking";
-    private const string allAsSplitAsNoTracking = "https://localhost:44359/Products/allAsSplitAsNoTracking";
 
     [Benchmark]
     public async Task AllSimple()
     {
-        await _httpClient.GetAsync(allSimple);
+        await _httpClient.GetAsync("https://localhost:44359/Products/allSimple");
     }
     
-    [Benchmark]
-    public async Task AllAsSplitQuery()
-    {
-        await _httpClient.GetAsync(allAsSplitQuery);
-    }  
-    
-    [Benchmark]
-    public async Task AllAsNoTracking()
-    {
-        await _httpClient.GetAsync(allAsNoTracking);
-    }
+    // [Benchmark]
+    // public async Task AllAsSplitQuery()
+    // {
+    //     await _httpClient.GetAsync("https://localhost:44359/Products/allAsSplitQuery");
+    // }  
+    //
+    // [Benchmark]
+    // public async Task AllAsNoTracking()
+    // {
+    //     await _httpClient.GetAsync("https://localhost:44359/Products/allAsNoTracking");
+    // }
     
     [Benchmark]
     public async Task AllAsSplitAsNoTracking()
     {
-        await _httpClient.GetAsync(allAsSplitAsNoTracking);
+        await _httpClient.GetAsync("https://localhost:44359/Products/allAsSplitAsNoTracking");
     }
     
 
