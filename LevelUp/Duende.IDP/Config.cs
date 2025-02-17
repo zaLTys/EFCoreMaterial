c55861244ecb4d354c31338432922a1b9c43d8cb
Povilas Šimanskas <p.simanskas@adform.com>
2025-02-17 22:36:46 +0200

    Register client in IDP

--- LevelUp/Duende.IDP/Config.cs
+++ LevelUp/Duende.IDP/Config.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace Duende.IDP;
 
@@ -6,7 +7,7 @@ public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
         new IdentityResource[]
-        { 
+        {
             new IdentityResources.OpenId(), //user identifier
             new IdentityResources.Profile() //user profile
         };
@@ -16,6 +17,39 @@ public static class Config
             { };
 
     public static IEnumerable<Client> Clients =>
-        new Client[] 
-            { };
+        new Client[]
+        {
+            // Define a new client configuration for IdentityServer
+            new Client()
+            {
+                // The name of the client application
+                ClientName = "WebApp1",
+
+                // Unique identifier for the client
+                ClientId = "webapp1client",
+
+                // Specifies the authentication flow to be used
+                // 'Code' grant type is used for OAuth 2.0 authorization code flow
+                AllowedGrantTypes = GrantTypes.Code,
+
+                // List of allowed redirect URIs after authentication
+                RedirectUris =
+                {
+                    "https://localhost:7285/signin-oidc" // Redirect URI for OpenID Connect authentication
+                },
+
+                // Defines the scopes that this client can request
+                AllowedScopes =
+                {
+                    IdentityServerConstants.StandardScopes.OpenId, // Includes the 'sub' (subject) claim
+                    IdentityServerConstants.StandardScopes.Profile // Includes profile-related claims
+                },
+
+                // Defines the secret(s) associated with the client for authentication
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256()) // Hashed secret used for client authentication
+                }
+            }
+        };
 }
