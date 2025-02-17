ea229cc77264d2795155c6e512226ac7f53e83b8
Povilas Šimanskas <p.simanskas@adform.com>
2025-02-17 20:58:52 +0200

    Initial setup, test users, pre-login

---
+++ LevelUp/Duende.IDP/Config.cs
@@ -0,0 +1,21 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace Duende.IDP;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        { 
+            new IdentityResources.OpenId(), //user identifier
+            new IdentityResources.Profile() //user profile
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+            { };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[] 
+            { };
+}
